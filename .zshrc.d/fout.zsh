b41b332c5daf89f1e6642800b59660f7725530e0
doiken <doi_kenji@fout.jp>
2017-08-29 08:24:41 +0900

    fcp

--- .zshrc.d/fout.zsh
+++ .zshrc.d/fout.zsh
@@ -3,7 +3,11 @@
 ##
 alias fsh="ssh -t gw4 sudo -u nn ssh "
 export SSH="ssh -t gw4 sudo -u nn ssh " # for tssh
-
+function fcp () {
+  from=$1
+  to=$2
+  rsync -av -e 'ssh -oClearAllForwardings=yes gw4 sudo -u nn -i ssh' $from $to
+}
 ##
 ## Env
 ##
