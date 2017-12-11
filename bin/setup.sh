bbcded222317c163e4f5aacb628a7a3ade22a04c
doiken <doiken@users.noreply.github.com>
2017-12-11 11:01:53 +0900

    add bitbar

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,7 +8,20 @@
 ##
 ## Sym Link
 ##
-DOT_FILES=( .hammerspoon bin .zsh .zshrc .zshenv .zprofile .gitconfig .gitignore_global .vimrc .ideavimrc .tmux.conf $(echo $(cd ~/dotfiles/; echo .zshrc.d/*)) )
+DOT_FILES=(
+  .hammerspoon
+  bin
+  .zsh
+  .zshrc
+  .zshenv
+  .zprofile
+  .gitconfig
+  .gitignore_global
+  .vimrc .ideavimrc
+  .tmux.conf
+  $(echo $(cd ~/dotfiles/; echo .zshrc.d/*))
+)
+
 mkdir -p ~/.zshrc.d
 for file in ${DOT_FILES[@]}
 do
@@ -19,20 +32,36 @@ done
 ## Install Basics
 ##
 
-## install oh-my-zsh
-## [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
 ## Homebrew
 [ ! -x /usr/local/bin/brew ] && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-BREW_EXECS=( git hub tmux peco reattach-to-user-namespace cask argon/mas/mas terminal-notifier gnupg gnupg2 zplug rbenv ruby-build rbenv-gemset sqlparse)
+BREW_EXECS=(
+  git
+  hub
+  tmux
+  peco
+  reattach-to-user-namespace
+  cask
+  argon/mas/mas
+  terminal-notifier
+  gnupg
+  gnupg2
+  zplug
+  rbenv
+  ruby-build
+  rbenv-gemset sqlparse
+)
 for e in ${BREW_EXECS[@]}
 do
     brew list $e >/dev/null || brew install $e
 done
 brew install mysql --client-only
 
-CASK_EXECS=( google-play-music-desktop-player ngrok )
+CASK_EXECS=(
+  google-play-music-desktop-player
+  ngrok
+  bitbar
+)
 for e in ${CASK_EXECS[@]}
 do
     brew cask list $e >/dev/null || brew cask install $e
@@ -42,7 +71,6 @@ done
 ## Configure
 ##
 SCRIPTS=( ~/bin/key_repeat.sh )
-key_repeat.sh
 for script in ${SCRIPTS[@]}
 do
     [ -x $script ] && $script
