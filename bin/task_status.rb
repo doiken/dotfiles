ea6da3247cc078a0c9f6c1dd3525bd6eb49f2c2d
doiken <doiken@users.noreply.github.com>
2019-02-26 17:00:44 +0900

    task status

--- bin/task_status.rb
+++ bin/task_status.rb
@@ -9,13 +9,13 @@ require 'json'
 # a6140cbf6e84a0bAffb0cX49138fc5687310b518
 #   or
 # launchctl setenv REDMINE_ACCESS_TOKEN a6140cbf6e84a0bAffb0cX49138fc5687310b518
-token = ENV["REDMINE_ACCESS_TOKEN"] || 'd7f0f0482424b6925986e0fe679792e7944964ae'
+token = ENV["REDMINE_ACCESS_TOKEN"]
 # https://redmine.xxxx.com
 #   or
 # launchctl setenv REDMINE_URL https://redmine.xxxx.com
 $redmine_url = ENV["REDMINE_URL"] || 'https://redmine.fout.jp'
-limit = ENV["LIMIT"] || 5
 display = ARGV[0] # qiita/slack
+limit = ARGV[1] || 5
 
 def disp_qiita(issue)
   puts <<~EOS.strip
