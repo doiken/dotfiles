230c7e430b752d441b51648deab0b5509edeea70
doiken <doiken@users.noreply.github.com>
2019-03-07 20:28:16 +0900

    remove unused scripts

--- bin/dbox_setup.sh
+++
@@ -1,5 +0,0 @@
-##
-## for Atom
-##
-ln -s ~/Dropbox/data/tool/atom/keymap.cson ~/.atom/keymap.cson
-
--- bin/import-kafka.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka:0.8.2.0 /bin/bash -c "echo -n '`cat $3`' | \$KAFKA_HOME/bin/kafka-console-producer.sh --topic sample-topic --broker-list=\`broker-list.sh\`"
-
