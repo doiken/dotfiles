343bcd224d31812593edceaf2abcd8402852c7d6
doiken <doiken@users.noreply.github.com>
2019-08-20 12:48:22 +0900

    spoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,84 @@
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall.repos.doiken = {
+   url = "https://github.com/doiken/Spoons",
+   desc = "doiken's spoon repository",
+}
+spoon.SpoonInstall:andUse("PseudoNumKey", { repo = 'doiken', start = true })
+spoon.SpoonInstall:andUse("LastKeyRepeat", {
+  repo = 'doiken',
+  config = {
+    mapping = {
+      { first = { key = 'g', mods = {'ctrl'} }, second = { key = 'h' } },
+      { first = { key = 'g', mods = {'ctrl'} }, second = { key = 'l' } },
+      { first = { key = 'g', mors = {'ctrl'} }, second = { key = 'k' } },
+      { first = { key = 'g', mods = {'ctrl'} }, second = { key = 'j' } },
+    },
+    appsDisable = { "iTerm2" }
+  }
+})
+spoon.LastKeyRepeat:init():start()
+
+spoon.SpoonInstall:andUse("SwitchableHotkey", {
+  repo = 'doiken',
+  config = {
+    acceptOnly = {
+      -- app name = { idx1, ... }
+      ["iTerm2"] = {
+        {{'ctrl'}, ']'},
+      },
+      ["Emacs"] = {},
+      ["IntelliJ IDEA"] = {
+        {{'ctrl'}, ']'},
+        {{'ctrl'}, 'c'},
+        {{'ctrl'}, 'n'},
+        {{'ctrl'}, 'p'},
+        {{'ctrl'}, 'm'},
+        {{'ctrl'}, 'd'},
+        {{'ctrl'}, 'h'},
+      },
+      ["PyCharm"] = {
+        {{'ctrl'}, ']'},
+        {{'ctrl'}, 'n'},
+        {{'ctrl'}, 'p'},
+        {{'ctrl'}, 'm'},
+        {{'ctrl'}, 'd'},
+        {{'ctrl'}, 'h'},
+      },
+      ["Code"] = {
+        {{'ctrl'}, ']'},
+        {{'ctrl'}, 'c'},
+        {{'ctrl'}, 'm'},
+        {{'ctrl'}, 'd'},
+        {{'ctrl'}, 'h'},
+      },
+    }
+  }
+})
+
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'f'}, {'right'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'b'}, {'left'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'n'}, {'down'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'p'}, {'up'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'e'}, {'right', {'cmd'}})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'a'}, {'left', {'cmd'}})
+spoon.SwitchableHotkey:bindSpec({{'option'}, 'f'}, {'right', {'option'}})
+spoon.SwitchableHotkey:bindSpec({{'option'}, 'b'}, {'left', {'option'}})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'v'}, {'pagedown'})
+spoon.SwitchableHotkey:bindSpec({{'option'}, 'v'}, {'pageup'})
+
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, ']'}, {'escape'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'c'}, {'escape'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'm'}, {'return'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'd'}, {'forwarddelete'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'h'}, {'delete'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'u'}, {'delete', {'cmd'}})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'k'}, {'right', {'shift', 'cmd'}}, {'forwarddelete'})
+spoon.SwitchableHotkey:bindSpec({{'ctrl'}, 'w'}, {'delete', {'option'}})
+spoon.SwitchableHotkey:bindSpec({{'option'}, 'd'}, {'forwarddelete', {'option'}})
+-- SwitchableHotkey:bindSpec({{'option'}, 'h'}, {'delete', {'option'}))
+--SwitchableHotkey:bindSpec({{'option', 'cmd'}, 'r'}, function () hs.reload() end)
+spoon.SwitchableHotkey:init():start()
+
 --
 -- enhance fnutils
 --
@@ -24,4 +105,7 @@ keymaps = require("keymaps")
 
 monitor = require("monitor")
 monitor.start()
+
+-- for cli use
+require("hs.ipc")
 
--- .hammerspoon/keymaps.lua
+++ .hammerspoon/keymaps.lua
@@ -10,43 +10,6 @@ local log = logger.new(debug.getinfo(1,'S').source, 'debug')
 local module = {}
 
 --------------------------------------------------------------------------------
--- hotkey_switcher (must be last)
---------------------------------------------------------------------------------
-local hotKeySwithcer = require("hotkey_switcher")
-module.hotKeySwithcer = hotKeySwithcer
-hotKeySwithcer.remapKey({{'ctrl'}, ']'}, {'escape'})
-hotKeySwithcer.remapKey({{'ctrl'}, 'c'}, {'escape'})
-hotKeySwithcer.remapKey({{'ctrl'}, 'f'}, {'right'})
-hotKeySwithcer.remapKey({{'ctrl'}, 'b'}, {'left'})
-hotKeySwithcer.remapKey({{'ctrl'}, 'n'}, {'down'})
-hotKeySwithcer.remapKey({{'ctrl'}, 'p'}, {'up'})
-
-hotKeySwithcer.remapKey({{'ctrl'}, 'e'}, {'right', {'cmd'}})
-hotKeySwithcer.remapKey({{'ctrl'}, 'a'}, {'left', {'cmd'}})
-
-hotKeySwithcer.remapKey({{'ctrl'}, 'm'}, {'return'})
-hotKeySwithcer.remapKey({{'ctrl'}, 'd'}, {'forwarddelete'})
-hotKeySwithcer.remapKey({{'ctrl'}, 'h'}, {'delete'})
-hotKeySwithcer.remapKey({{'ctrl'}, 'u'}, {'delete', {'cmd'}})
-hotKeySwithcer.remapKey({{'ctrl'}, 'k'}, {'right', {'shift', 'cmd'}}, {'forwarddelete'})
-
-hotKeySwithcer.remapKey({{'ctrl'}, 'w'}, {'delete', {'option'}})
-
--- hotKeySwithcer.remapKey({{'ctrl'}, 'y'}, {'v', {'cmd'}))
--- hotKeySwithcer.remapKey({{'ctrl'}, '/'}, {'z', {'cmd'}))
-
-hotKeySwithcer.remapKey({{'option'}, 'f'}, {'right', {'option'}})
-hotKeySwithcer.remapKey({{'option'}, 'b'}, {'left', {'option'}})
-hotKeySwithcer.remapKey({{'option'}, 'd'}, {'forwarddelete', {'option'}})
--- hotKeySwithcer.remapKey({{'option'}, 'h'}, {'delete', {'option'}))
-
-hotKeySwithcer.remapKey({{'ctrl'}, 'v'}, {'pagedown'})
-hotKeySwithcer.remapKey({{'option'}, 'v'}, {'pageup'})
---hotKeySwithcer.remapKey({{'option', 'cmd'}, 'r'}, function () hs.reload() end)
-hotKeySwithcer.init()
-hotKeySwithcer.start()
-
---------------------------------------------------------------------------------
 -- layout bind
 --------------------------------------------------------------------------------
 local remapKey = function (modifiers, key, command)
@@ -68,18 +31,6 @@ else
 end
 module.windowLayout = windowLayout
 
---------------------------------------------------------------------------------
--- last key repeat
---------------------------------------------------------------------------------
-local lastKeyRepeat = require("last_key_repeat")
-lastKeyRepeat.mapping = {
-  { first = { key = 'g', mods = {'ctrl'} }, second = { key = 'h' } },
-  { first = { key = 'g', mods = {'ctrl'} }, second = { key = 'l' } },
-  { first = { key = 'g', mods = {'ctrl'} }, second = { key = 'k' } },
-  { first = { key = 'g', mods = {'ctrl'} }, second = { key = 'l' } },
-}
-lastKeyRepeat.init().start()
-module.lastKeyRepeat = lastKeyRepeat
 
 --------------------------------------------------------------------------------
 -- modmaps
@@ -100,14 +51,3 @@ module.lastKeyRepeat = lastKeyRepeat
   module.modmaps = modmaps
 -- end
 
---------------------------------------------------------------------------------
--- pseudo_num_key
---------------------------------------------------------------------------------
-local PseudoNumKey = require("pseudo_num_key")
-PseudoNumKey.num_maps = {
-    ["u"] = "7", ["i"] = "8", ["o"] = "9",
-    ["j"] = "4", ["k"] = "5", ["l"] = "6",
-    ["m"] = "1", [","] = "2", ["."] = "3",
-    ["space"] = "0", 
-}
-PseudoNumKey.start()
