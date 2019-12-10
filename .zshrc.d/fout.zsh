c63e1ad608a52b84a524159eb4928c413dca4932
doiken <doiken@users.noreply.github.com>
2019-12-10 15:45:51 +0900

    add db_tunnel

--- .zshrc.d/fout.zsh
+++ .zshrc.d/fout.zsh
@@ -5,6 +5,9 @@ export SSH_STEP_SERVER=gw4
 function fsh () {
   ssh -t ${SSH_STEP_SERVER} "sudo -u nn ssh $@"
 }
+function db_tunnel () {
+  ssh -tL 3306:127.0.0.1:13306 gw4 sudo -u nn ssh -tL 13306:127.0.0.1:3306 dbbackup
+}
 # alias fsh="ssh -t ${SSH_STEP_SERVER} sudo -u nn ssh "
 export SSH="ssh -t ${SSH_STEP_SERVER} sudo -u nn ssh " # for tssh
 function fcp () {
