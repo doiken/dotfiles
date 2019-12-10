d2896110f85d1b5957b862525ead3e18f361d214
doiken <doiken@users.noreply.github.com>
2019-12-10 16:03:29 +0900

    implement redmine watcher

---
+++ .hammerspoon/RedmineWatcher.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# NAME
+#   RedmineWatcher.sh -- fetch issues
+# USAGE
+#   RedmineWatcher.sh PATH_TO_ENVFILE
+# DESCRIPTION
+#   following env var should be defined in PATH_TO_ENVFILE
+#     - REDMINE_ACCESS_TOKEN
+#     - REDMINE_URL
+#     - REDMINE_USER_ID
+#   jq command required
+source $1
+commands=(
+    "'${REDMINE_URL}/issues.json?key=${REDMINE_ACCESS_TOKEN}&limit=100&status_id=open&assigned_to_id=${REDMINE_USER_ID}'| /usr/local/bin/jq '.issues[] | {id:.id|tostring, title:.subject, url: ("\""${REDMINE_URL}/issues/"\"" + (.id|tostring)), updated_at: .updated_on, type: "\""mine"\""}'"
+    "'${REDMINE_URL}/issues.json?key=${REDMINE_ACCESS_TOKEN}&limit=10&status_id=open&watcher_id=${REDMINE_USER_ID}&sort=updated_on:desc'| /usr/local/bin/jq '.issues[] | {id:.id|tostring, title:.subject, url: ("\""${REDMINE_URL}/issues/"\"" + (.id|tostring)), updated_at: .updated_on, type: "\""watch"\""}'"
+)
+
+stdout=""
+for cmd in "${commands[@]}" ; do
+    stdout="${stdout} $(eval curl -s $cmd)"
+done
+
+echo $stdout | /usr/local/bin/jq . --slurp
