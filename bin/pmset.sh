6f870b56da12720501dcb45de351a21342be9107
doiken <doiken@users.noreply.github.com>
2020-02-13 21:52:21 +0900

    modify pmset

--- bin/pmset.sh
+++ bin/pmset.sh
@@ -3,14 +3,22 @@
 # NAME
 #   pmset.sh
 # USAGE
-#   pmset.sh [STANDBY_DELAY_HOUR]
+#   pmset.sh HIBERNATEMODE STANDBY_DELAY_HOUR
 # DESCRIPTION
 #   set pmset params
-
+#   ref: man pmset
+#   ref: https://apple.stackexchange.com/questions/113954/difference-between-autopoweroff-and-standby-in-pmset
 # ok to keep default
 # lidwake              0
 # powernap             0
 # hibernatemode        3
-standby_delay_hour=${1:-1}
+hibernatemode=$1
+standby_delay_hour=$2
 
-sudo pmset -a powernap 0 standbydelaylow $(($standby_delay_hour * 60 * 60)) standbydelayhigh $(($standby_delay_hour * 60 * 60)) 
+sudo pmset -a \
+  hibernatemode $hibernatemode \
+  sleep 60 \
+  powernap 0 \
+  standbydelaylow $((1 + $standby_delay_hour * 60 * 60)) \
+  standbydelayhigh $((1 + $standby_delay_hour * 60 * 60))
+
