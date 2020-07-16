7031369c442c163993a01f15206fd9936e470e38
doiken <doiken@users.noreply.github.com>
2020-07-16 15:51:43 +0900

    implement hankaku2zenkaku

---
+++ bin/hankaku2zenkaku.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# ref: https://orebibou.com/2018/04/%E3%82%B3%E3%83%B3%E3%82%BD%E3%83%BC%E3%83%AB%E4%B8%8A%E3%81%A7%E3%82%A2%E3%83%AB%E3%83%95%E3%82%A1%E3%83%99%E3%83%83%E3%83%88%E3%83%BB%E6%95%B0%E5%AD%97%E3%81%AE%E5%8D%8A%E8%A7%92%E2%87%94%E5%85%A8/
+sed 'y/ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ０１２３４５６７８９．/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./'
