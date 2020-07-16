a7e5539a5d55b6b3aa82c99ec4e5d45f8bafceec
doiken <doiken@users.noreply.github.com>
2020-07-16 15:54:38 +0900

    misc

--- bin/space2table.pl
+++ bin/space2table.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/env perl
 #
 # NAME
-#   space2table.pl -- space or tab separated column to table format
+#   space2table.pl -- space or tab separated columns to table format
 # USAGE
-#   space2table.pl
+#   pbpaste | space2table.pl [textile or markdown] | pbcopy
 # DESCRIPTION
-#   words not accepted: |, \t
+#   words not accepted: " |\t"
 my %parse_1st_line_func = (
     textile => sub {
         my ($separator, $line) = @_;
         $line =~ s/$separator/ |_. /g;
-        return "|_. $line |\n";
+        return "{background:#E6E6E6}. |_. $line |\n";
     },
     markdown => sub {
         my ($separator, $line) = @_;
@@ -29,6 +29,7 @@ while (<STDIN>){
     chomp;
     if ($i++ == 0) {
         $line = $_;
+        # 区切り文字の判定は先頭行のtab区切りの有無
         $separator = ($line =~ /\t/) ? "\t" : " {2,}";
         $line = $parse_1st_line_func{$type}($separator, $line);
     } else {
--- bin/textile_space2table.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-perl -lne 'BEGIN { my $i = 0 } { my $prefix = $i++ == 0 ? "_." : ""; s/\t/ |$prefix /g; print "|$prefix $_ |" }'
