b5d2deb27faf9294b54fed9237e7ce61d1122809
doiken <doiken@users.noreply.github.com>
2021-01-19 18:54:21 +0900

    set LANG

--- bin/run_copy.sh
+++ bin/run_copy.sh
@@ -3,4 +3,7 @@
 # run shell and copy result
 cmd=$(cd $(dirname $1) && pwd)/$(basename $1)
 shift
-$cmd "$@" | pbcopy
+# set LANG to call via IntelliJ IDEA external tools
+# man pbcopy
+# > pbcopy  and pbpaste use locale environment variables to determine the encoding to be used for input and output
+$cmd "$@" | LANG=ja_JP.UTF-8 pbcopy
