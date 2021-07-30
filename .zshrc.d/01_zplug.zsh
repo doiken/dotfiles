6c406bc415709420e2a9e1ebf1d0152f2a0a4a9b
doiken <doiken@users.noreply.github.com>
2021-07-30 10:59:43 +0900

    remove gh-r

--- .zshrc.d/01_zplug.zsh
+++ .zshrc.d/01_zplug.zsh
@@ -9,6 +9,5 @@ zplug 'zsh-users/zsh-autosuggestions'
 zplug 'zsh-users/zsh-completions'
 zplug 'zsh-users/zsh-syntax-highlighting', defer:3
 zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
-zplug "b4b4r07/gist", from:gh-r, as:command, use:"*darwin*amd64*"
 
 zplug load
