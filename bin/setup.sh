8ce203d22e9159ebc34cf8bb02857214b2b2efac
doiken <doiken@users.noreply.github.com>
2022-12-26 11:10:21 +0900

    feat: update setup scripts

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,6 +9,7 @@
 ## Sym Link
 ##
 DOT_FILES=(
+  .Brewfile
   .hammerspoon
   bin
   .zsh
@@ -38,61 +39,7 @@ done
 
 ## Homebrew
 [ ! -x /usr/local/bin/brew ] && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-BREW_EXECS=(
-  git
-  hub
-  tmux
-  reattach-to-user-namespace
-  terminal-notifier
-  zplug
-  peco
-  jq
-  python3
-  ndenv
-  pipenv
-  ctags
-  # heavy app had better to be last
-  rbenv
-  ruby-build
-  mysql
-)
-{
-  PATTERN="$(brew list | xargs echo | perl -pe 's/ /|/g')"
-  for e in ${BREW_EXECS[@]}; do
-    [ "$(egrep -v $PATTERN <(echo $e))" != "" ] && brew install $e
-  done
-} &
-CASK_EXECS=(
-  google-chrome
-  google-japanese-ime
-  iterm2
-  hammerspoon
-  ngrok
-  visual-studio-code
-  keycastr # print what you type
-  licecap  # capture as gif
-  skitch
-  slack
-  pycharm-ce
-  intellij-idea-ce
-  mapture
-  # heavy app had better to be last
-  vagrant
-  java
-  virtualbox
-  docker
-  font-jetbrains-mono
-  font-ricty-diminished
-  biscuit
-)
-{
-  brew tap homebrew/cask-fonts
-  PATTERN="$(brew list --cask | xargs echo | perl -pe 's/ /|/g')"
-  for e in ${CASK_EXECS[@]}; do
-      [ "$(egrep -v $PATTERN <(echo $e))" != "" ] && brew install $e --cask
-  done
-} &
+brew bundle --global &
 
 ##
 ## Configure
@@ -106,9 +53,9 @@ done
 ## docker completion
 ## https://docs.docker.com/docker-for-mac/
 ##
-[ ! -e /usr/local/share/zsh/site-functions/_docker ] && ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
-[ ! -e /usr/local/share/zsh/site-functions/_docker-machine ] && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
-[ ! -e /usr/local/share/zsh/site-functions/_docker-compose ] && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
+# [ ! -e /usr/local/share/zsh/site-functions/_docker ] && ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
+# [ ! -e /usr/local/share/zsh/site-functions/_docker-machine ] && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-machine.zsh-completion /usr/local/share/zsh/site-functions/_docker-machine
+# [ ! -e /usr/local/share/zsh/site-functions/_docker-compose ] && ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
 
 wait
 
