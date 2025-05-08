d3c3c19f3d96c3f5ec36b4fa55b377aaa5e57ed8
doiken <doiken@users.noreply.github.com>
2025-05-08 12:17:51 +0900

    chore: modify commands about ssh operation

--- .zshrc.d/fout.zsh
+++ .zshrc.d/fout.zsh
@@ -1,24 +1,33 @@
 ##
 ## Alias
 ##
-export SSH_STEP_SERVER=gw
+function _f_step () {
+  # 社内サーバなら SSH_STEP_SERVER を更新
+  first_arg=$(printf "%s\n" "$@" | grep -E -m 1 -v '^-')
+  if [[ "$first_arg" == fjord* || "$first_arg" == ukigumo* ]]; then
+    echo xp0_global
+  else
+		echo gw
+  fi
+}
 function fsh () {
-  ssh -t ${SSH_STEP_SERVER} "sudo -u nn ssh $@"
+  step_server=$(_f_step "$@")
+  ssh -t ${step_server} "sudo -u nn ssh $@"
 }
 function db_tunnel () {
   ssh -tL 3306:127.0.0.1:13306 gw4 sudo -u nn ssh -tL 13306:127.0.0.1:3306 dbbackup
 }
-# alias fsh="ssh -t ${SSH_STEP_SERVER} sudo -u nn ssh "
-export SSH="ssh -t ${SSH_STEP_SERVER} sudo -u nn ssh " # for tssh
 function fcp () {
   from=$1
   to=$2
-  rsync -av -e "ssh -oClearAllForwardings=yes ${SSH_STEP_SERVER} sudo -u nn -i ssh" $from $to
+  step_server=$(_f_step "$@")
+  rsync -av -e "ssh -oClearAllForwardings=yes ${step_server} sudo -u nn -i ssh" $from $to
 }
 ##
 ## Env
 ##
 export FOUT_HOME=/fout/fout/
+export PATH="${HOMEBREW_PREFIX}/opt/mysql-client@8.0/bin:$PATH"
 funciton bsh () {
   ssh-add ~/.ssh/id_rsa_bastion
   ssh -At gw "ssh bastion $@"
