8efeb628f025ce23dc387a104ec2d3bed752a92c
doiken <doiken@users.noreply.github.com>
2025-05-15 19:50:05 +0900

    chore:add alias for less

--- .zshrc.d/common.zsh
+++ .zshrc.d/common.zsh
@@ -108,6 +108,7 @@ setopt transient_rprompt
 #
 # Exports
 #
+export LESS="-iRMXS"
 export CLICOLOR=true
 export LSCOLORS='exfxcxdxbxGxDxabagacad'
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
